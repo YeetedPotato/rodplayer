3857278a26ab3130fe7fa72849a9661651cc04e3
YeetedPotato <96884066+YeetedPotato@users.noreply.github.com>
2026-09-11 13:49:46 -0500

    fix: remove unused import media_kit_video and clean analyzer warnings in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
-import 'package:media_kit_video/media_kit_video.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/api/remux_client.dart';
