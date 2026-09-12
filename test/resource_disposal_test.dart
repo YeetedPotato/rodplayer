045ec741a9f47d6e7334a5a14beeed65cbd36d2e
YeetedPotato <96884066+YeetedPotato@users.noreply.github.com>
2026-09-12 07:18:09 -0500

    test: verify player resource disposal

---
+++ test/resource_disposal_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rodplayer/core/player/player_controller.dart';
+
+void main() {
+  test('dispose cancels subscriptions and disposes state notifiers', () async {
+    final engine = RodPlayerEngine();
+    final playingValues = <bool>[];
+    final bufferingValues = <bool>[];
+    final errors = <String?>[];
+
+    engine.playing.addListener(() => playingValues.add(engine.playing.value));
+    engine.buffering.addListener(() => bufferingValues.add(engine.buffering.value));
+    engine.error.addListener(() => errors.add(engine.error.value));
+
+    await engine.dispose();
+    await engine.dispose();
+
+    expect(() => engine.playing.value = true, throwsA(isA<FlutterError>()));
+    expect(() => engine.buffering.value = true, throwsA(isA<FlutterError>()));
+    expect(() => engine.error.value = 'after dispose', throwsA(isA<FlutterError>()));
+    expect(playingValues, isEmpty);
+    expect(bufferingValues, isEmpty);
+    expect(errors, isEmpty);
+  });
+}
